1ce46a66ca23d1d2cda250f23fab9a67b2b32666
Kush Vasaniya <vasaniyakush@gmail.com>
2023-09-08 17:31:11 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ export default function Home() {
 
   return (
     <div>
-     <span onMouseEnter={() => setShowTooltip(true)} onMouseLeave={() => setShowTooltip(false)}>Hover over this text to see the dynamic tooltip.</span>
+     <span >Hover over this text to see the dynamic tooltip.</span>
       {showTooltip && <DynamicTooltip />}
     </div>
   );
